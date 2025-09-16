9f482afad22a079aecf52a06ae6b4fad49331c98
Tej-as-01 <t32345839@gmail.com>
2025-09-16 18:15:21 +0530

    Update BlogsController.java

--- src/main/java/com/example/demo/controller/BlogsController.java
+++ src/main/java/com/example/demo/controller/BlogsController.java
@@ -17,38 +17,44 @@ import com.example.demo.ResourceNotFoundException;
 import com.example.demo.repo.Blogs;
 import com.example.demo.service.BlogsService;
 
-
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 @RestController
 @RequestMapping("/blogs")
-
+@Tag(name = "Blogs", description = "Operations related to blog posts")
 public class BlogsController {
 
 	@Autowired
 	private BlogsService blogservice;
 
 	@GetMapping()
+	@Operation(summary="Getting all Blogs")
 	public List<Blogs> getAllBlogs() {
 		return blogservice.getAllBlogs();
 	}
 
 	@GetMapping("/{id}")
+	@Operation(summary="Getting Blogs by ID")
 	public Blogs getById(@PathVariable("id") Long id) throws ResourceNotFoundException {
 		return blogservice.getById(id);
 	}
 
-@PostMapping("/post")
+	@PostMapping("/post")
+	@Operation(summary="Posting the Blogs")
 	public ResponseEntity<Blogs> createBlogs(@RequestBody Blogs blog) {
 		Blogs blogs=blogservice.createBlogs(blog);
 		return ResponseEntity.status(HttpStatus.CREATED).body(blogs);
 	}
 
 	@DeleteMapping("/delete/{id}")
+	@Operation(summary="Deleting Blogs by ID")
 	public void deleteById(@PathVariable("id") Long id) throws ResourceNotFoundException {
 		blogservice.deleteById(id);
 	}
 
 	@DeleteMapping("/delete")
+	@Operation(summary="Deleting all Blogs")
 	public void deleteAll() {
 		blogservice.deleteAll();
 	}
