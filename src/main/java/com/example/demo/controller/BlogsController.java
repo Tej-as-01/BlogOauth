79660b961163ad40e555c89276118cac86047557
Tej-as-01 <t32345839@gmail.com>
2025-09-18 16:49:36 +0530

    Update BlogsController.java

--- src/main/java/com/example/demo/controller/BlogsController.java
+++ src/main/java/com/example/demo/controller/BlogsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -47,6 +48,13 @@ public class BlogsController {
 		return ResponseEntity.status(HttpStatus.CREATED).body(blogs);
 	}
 
+	@PutMapping("/update/{id}")
+	@Operation(summary="Updating the Blogs")
+	public void updateBlogs(@PathVariable("id") Long id, @RequestBody Blogs blog)
+	{
+		blogservice.updateBlogs(id,blog);
+	}
+	
 	@DeleteMapping("/delete/{id}")
 	@Operation(summary="Deleting Blogs by ID")
 	public void deleteById(@PathVariable("id") Long id) throws ResourceNotFoundException {
